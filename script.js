82f2ca4eda043181cd9e5f3c0182edc8ee53fdd4
Kasim Shaikh <kasimshaikh1337@gmail.com>
2023-06-08 16:57:43 +0530

    speed length fix

--- script.js
+++ script.js
@@ -10,42 +10,32 @@ const options = {
   timeout: 5000,
   maximumAge: 0,
 };
-navigator.geolocation.watchPosition(successCallback, errorCallback, options);
-var reqcount = 0;
-function successCallback(pos) {
-  const crd = pos.coords;
-  lat.innerHTML = crd.latitude;
-  long.innerHTML = crd.longitude;
-  accuracy.innerHTML = crd.accuracy.toFixed(2);
-  speed.innerHTML = crd.speed.toFixed(3);
-  date.innerHTML =
-    new Date(pos.timestamp).toLocaleTimeString() +
-    " " +
-    new Date(pos.timestamp).toLocaleDateString();
-  reqcount++;
-  count.innerHTML = reqcount;
-  // set lat and long to local storage
-  localStorage.setItem("lat", crd.latitude);
-  localStorage.setItem("long", crd.longitude);
-  localStorage.setItem("acc", crd.accuracy.toFixed(2));
-  localStorage.setItem("speed", crd.speed).toFixed(3);
-  localStorage.setItem(
-    "date",
-    new Date(pos.timestamp).toLocaleTimeString() +
+if (navigator.geolocation) {
+  var reqcount = 0;
+  navigator.geolocation.watchPosition(successCallback, errorCallback, options);
+  function successCallback(pos) {
+    const crd = pos.coords;
+    lat.innerHTML = crd.latitude;
+    long.innerHTML = crd.longitude;
+    accuracy.innerHTML = crd.accuracy.toFixed(2);
+    speed.innerHTML = crd.speed?.toFixed(2) || "0";
+    date.innerHTML =
+      new Date(pos.timestamp).toLocaleTimeString() +
       " " +
-      new Date(pos.timestamp).toLocaleDateString()
-  );
-  localStorage.setItem("count", reqcount);
-
-  fetch(
-    `https://geocode.maps.co/reverse?lat=${crd.latitude}&lon=${crd.longitude}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById("address").innerHTML = data.display_name;
-      localStorage.setItem("address", data.display_name);
-    });
-}
-function errorCallback(error) {
-  console.warn(`ERROR(${error.code}): ${error.message}`);
+      new Date(pos.timestamp).toLocaleDateString();
+    reqcount++;
+    count.innerHTML = reqcount;
+    fetch(
+      `https://geocode.maps.co/reverse?lat=${crd.latitude}&lon=${crd.longitude}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        document.getElementById("address").innerHTML = data.display_name;
+        localStorage.setItem("address", data.display_name);
+      });
+  }
+} else {
+  function errorCallback(error) {
+    console.warn(`ERROR(${error.code}): ${error.message}`);
+  }
 }
